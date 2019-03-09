416b78d128b227569c11a920dc907289f842b1a1
huahang <huahang.liu@gmail.com>
2019-03-09 12:56:54 +0800

    better error handling
    Signed-off-by: huahang <huahang.liu@gmail.com>

--- FixPermissions.go
+++ FixPermissions.go
@@ -6,6 +6,13 @@ import (
 	"path/filepath"
 )
 
+func checkError(err error) error {
+	if err != nil {
+		fmt.Printf("[Error] Hit an error! " + err.Error() + "\n")
+	}
+	return err
+}
+
 func main() {
 	args := os.Args
 	if len(args) != 2 {
@@ -14,16 +21,21 @@ func main() {
 	}
 	_ = filepath.Walk(args[1], func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			fmt.Printf("[Error] Hit an error! " + err.Error() + "\n")
-			return err
+			return checkError(err)
 		}
 		if info.Mode().IsDir() && info.Mode().Perm() != 0755 {
 			fmt.Printf("[Fixing] 0755 %s\n", path)
 			err = os.Chmod(path, 0755)
+			if err != nil {
+				return checkError(err)
+			}
 		}
 		if info.Mode().IsRegular() && info.Mode().Perm() != 0644 {
 			fmt.Printf("[Fixing] 0644 %s\n", path)
 			err = os.Chmod(path, 0644)
+			if err != nil {
+				return checkError(err)
+			}
 			return nil
 		}
 		return nil
